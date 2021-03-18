1a9fcfc3225aa322050d961992ae5c94f139e776
Geralt from Rivia <ctvbxm@mail.ru>
2021-03-18 23:17:46 +0200

    commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from PyQt5 import QtWidgets, QtGui, QtCore
+import sqlite3
+from contacts import *
+from join import *
+from student_room import *
+import sys
+import os

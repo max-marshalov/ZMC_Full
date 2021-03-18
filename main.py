c85bd40f5e8572e6fc44044ca461c54ad56506d0
Geralt from Rivia <ctvbxm@mail.ru>
2021-03-18 23:50:03 +0200

    вход

--- main.py
+++ main.py
@@ -2,8 +2,196 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import Qt
 from PyQt5 import QtWidgets, QtGui, QtCore
 import sqlite3
-from contacts import *
+from reg import *
 from join import *
-from student_room import *
 import sys
 import os
+from menu import *
+
+
+class Reg(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_Reg()
+        self.ui.setupUi(self)
+
+        self.ui.label_error.hide()
+
+        self.ui.pushButton.clicked.connect(self.go_back)
+        self.ui.btn_reg.clicked.connect(self.register)
+
+    def go_back(self):
+        try:
+            self.win = Join(self)
+            self.close()
+            self.win.show()
+
+        except Exception as er:
+            print(er)
+
+    def register(self):
+        Name = None
+        Surname = None
+        Otch = None
+        Sex = None
+        Login = None
+        Password = None
+
+        if len(self.ui.edit_name.text()) > 0:
+            Name = self.ui.edit_name.text()
+        else:
+            self.ui.label_error.show()
+            return
+
+        if len(self.ui.edit_surname.text()) > 0:
+            Surname = self.ui.edit_surname.text()
+        else:
+            self.ui.label_error.show()
+            return
+
+        if len(self.ui.edit_dad.text()) > 0:
+            Otch = self.ui.edit_dad.text()
+        else:
+            self.ui.label_error.show()
+            return
+
+        if len(self.ui.edit_mail.text()) > 0:
+            Login = self.ui.edit_mail.text()
+        else:
+            self.ui.label_error.show()
+            return
+
+        if len(self.ui.edit_password.text()) > 0:
+            Password = self.ui.edit_password.text()
+        else:
+            self.ui.label_error.show()
+            return
+
+        radio_base = [self.ui.radioButton_female, self.ui.radioButton_male]
+        for rad in radio_base:
+            if rad.isChecked():
+                Sex = rad.text()
+
+        if not Sex:
+            self.ui.label_error.show()
+            return
+
+        try:
+            con = sqlite3.connect("DATABASE.db")
+            curs = con.cursor()
+            curs.execute(
+                f"""INSERT INTO UserForm(name, surname, otchestvo, password, email, sex) VALUES("{Name}", "{Surname}", "{Otch}", "{Password}", "{Login}", "{Sex}") """)
+            con.commit()
+            con.close()
+        except sqlite3.IntegrityError:
+            print("Этот email уже используется")
+
+        try:
+            self.win = Join(self)
+            self.close()
+            self.win.show()
+
+        except Exception as er:
+            print(er)
+
+
+class Join(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_Join()
+        self.ui.setupUi(self)
+
+        self.ui.label_error.hide()
+
+        self.ui.btn_join.clicked.connect(self.go_join)
+        self.ui.btn_reg.clicked.connect(self.go_reg)
+
+    def go_join(self):
+        Login = None
+        Password = None
+
+        if len(self.ui.edit_login.text()) > 0:
+            Login = self.ui.edit_login.text()
+        else:
+            self.ui.label_error.setText("Введите логин и пароль")
+            self.ui.label_error.show()
+            return
+
+        if len(self.ui.edit_password.text()) > 0:
+            Password = self.ui.edit_password.text()
+        else:
+            self.ui.label_error.setText("Введите логин и пароль")
+            self.ui.label_error.show()
+            return
+
+        con = sqlite3.connect("DATABASE.db")
+        curs = con.cursor()
+        ex = curs.execute(
+            """SELECT * FROM UserForm WHERE email = "{}" and password = "{}" """.format(Login, Password)).fetchone()
+        con.commit()
+        con.close()
+        if not ex:
+            self.ui.label_error.setText("Неверный логин или пароль")
+            self.ui.label_error.show()
+            return
+        else:
+            try:
+                self.win = Menu(self, person=ex)
+                self.close()
+                self.win.show()
+
+            except Exception as er:
+                print(er)
+
+    def go_reg(self):
+        try:
+            self.win = Reg(self)
+            self.close()
+            self.win.show()
+
+        except Exception as er:
+            print(er)
+
+
+class Menu(QtWidgets.QMainWindow):
+    def __init__(self, parent=None, person=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui = Ui_Menu()
+        self.ui.setupUi(self)
+
+        self.con = sqlite3.connect("DATABASE.db")
+        self.curs = self.con.cursor()
+
+        self.person = person
+        print(self.person)
+
+        self.ui.pushButton.clicked.connect(self.go_zap)
+        self.ui.pushButton_2.clicked.connect(self.go_watch)
+        self.ui.pushButton_3.clicked.connect(self.go_change)
+
+        self.ui.label.setText(self.person[1])
+
+    def go_zap(self):
+        try:
+            self.win = Zap_Step1(self, person=self.person)
+            self.close()
+            self.win.show()
+
+        except Exception as er:
+            print(er)
+
+    def go_watch(self):
+        pass
+
+    def go_change(self):
+        pass
+
+
+if __name__ == '__main__':
+    try:
+        app = QtWidgets.QApplication(sys.argv)
+        myapp = Join()
+        myapp.show()
+        sys.exit(app.exec_())
+    except Exception as ex:
+        print(ex)
